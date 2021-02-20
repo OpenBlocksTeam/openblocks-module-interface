a0d83abebbeb9fdd46a6b29ea1d9cb07b4bdd76d
Iyxan23 <nurihsanalghifari@gmail.com>
2021-02-20 15:41:23 +0700

    add: Add setIndeterminate

--- lib/src/main/java/com/openbloks/communicator/callbacks/ProgressCallbacks.java
+++ lib/src/main/java/com/openbloks/communicator/callbacks/ProgressCallbacks.java
@@ -2,6 +2,7 @@ package com.openbloks.communicator.callbacks;
 
 public interface ProgressCallbacks {
     void onStart();
+    void setIndeterminate(boolean indeterminate);
     void setProgress(int max, int progress);
     void setProgressMessage(String message);
     void onFinished();
