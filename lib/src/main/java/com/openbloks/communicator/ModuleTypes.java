5b3ba7c97cc4466184ea368af2b341cc27c5a71e
Iyxan23 <nurihsanalghifari@gmail.com>
2021-02-20 15:21:17 +0700

    refactor: Rename project types to avoid misunderstanding

--- lib/src/main/java/com/openbloks/communicator/ModuleTypes.java
+++ lib/src/main/java/com/openbloks/communicator/ModuleTypes.java
@@ -3,7 +3,7 @@ package com.openbloks.communicator;
 public enum ModuleTypes {
     PROJECT_MANAGER,
     PROJECT_PARSER,
-    PROJECT_LAYOUT_VIEW,
-    PROJECT_LOGIC_VIEW,
+    PROJECT_LAYOUT_GUI,
+    PROJECT_CODE_GUI,
     PROJECT_COMPILER
 }
