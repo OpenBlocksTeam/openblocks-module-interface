fe1b4659b68c6e31ef839a22fe9ad0152c8b2043
Iyxan23 <31884435+Iyxan23@users.noreply.github.com>
2021-02-22 08:06:16 +0700

    docs: Add javadoc

--- lib/src/main/java/com/openblocks/moduleinterface/models/OpenBlocksFile.java
+++ lib/src/main/java/com/openblocks/moduleinterface/models/OpenBlocksFile.java
@@ -1,5 +1,8 @@
 package com.openblocks.moduleinterface.models;
 
+/**
+ * This class is basically used to store a file's data with the name on it, nothing special.
+ */
 public class OpenBlocksFile {
     public byte[] data;
     public String name;
