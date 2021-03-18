f53f73a8b755ed3c644e3496ea4ce0bafc3c03c8
Iyxan23 <nurihsanalghifari@gmail.com>
2021-03-18 15:34:34 +0700

    refactor: Change ArrayList<HashMap<>> to HashMap<>

--- lib/src/main/java/com/openblocks/moduleinterface/projectfiles/OpenBlocksCode.java
+++ lib/src/main/java/com/openblocks/moduleinterface/projectfiles/OpenBlocksCode.java
@@ -6,15 +6,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 // TODO: 3/18/21 Should be possible to make multiple collection of blocks / activity
+// TODO: 3/18/21 Customizable code template
 public class OpenBlocksCode {
-    public ArrayList<HashMap<String, String>> code_templates;
+    public HashMap<String, String> code_templates;
     public ArrayList<BlockCode> blocks = new ArrayList<>();
 
-    public OpenBlocksCode(ArrayList<HashMap<String, String>> code_templates) {
+    public OpenBlocksCode(HashMap<String, String> code_templates) {
         this.code_templates = code_templates;
     }
 
-    public OpenBlocksCode(ArrayList<HashMap<String, String>> code_templates, ArrayList<BlockCode> blocks) {
+    public OpenBlocksCode(HashMap<String, String> code_templates, ArrayList<BlockCode> blocks) {
         this.code_templates = code_templates;
         this.blocks = blocks;
     }
