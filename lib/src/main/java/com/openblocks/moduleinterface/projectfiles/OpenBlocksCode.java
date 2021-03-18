7a9ea90d830b09acb442376b10a9f437de5d5530
Iyxan23 <nurihsanalghifari@gmail.com>
2021-03-18 14:04:18 +0700

    fix: Fix NPE when accessing blocks

--- lib/src/main/java/com/openblocks/moduleinterface/projectfiles/OpenBlocksCode.java
+++ lib/src/main/java/com/openblocks/moduleinterface/projectfiles/OpenBlocksCode.java
@@ -5,9 +5,10 @@ import com.openblocks.moduleinterface.models.code.BlockCode;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+// TODO: 3/18/21 Should be possible to make multiple collection of blocks / activity
 public class OpenBlocksCode {
     public ArrayList<HashMap<String, String>> code_templates;
-    public ArrayList<BlockCode> blocks;
+    public ArrayList<BlockCode> blocks = new ArrayList<>();
 
     public OpenBlocksCode(ArrayList<HashMap<String, String>> code_templates) {
         this.code_templates = code_templates;
