b882a79eb1d7ba09402fdface97171b129b4d9cc
Iyxan23 <nurihsanalghifari@gmail.com>
2021-03-21 11:36:14 +0700

    fix: Fix incompatible class

--- lib/src/main/java/com/openblocks/moduleinterface/callbacks/Logger.java
+++ lib/src/main/java/com/openblocks/moduleinterface/callbacks/Logger.java
@@ -1,5 +1,7 @@
 package com.openblocks.moduleinterface.callbacks;
 
+import com.openblocks.moduleinterface.OpenBlocksModule;
+
 /**
  * This interface is used by modules to log stuff, a class that implements this
  * interface will be provided on module initialize.
@@ -7,10 +9,10 @@ package com.openblocks.moduleinterface.callbacks;
  * The logs will be displayed on the Logs tab of OpenBlocks
  */
 public interface Logger {
-    void debug  (Class<Object> module_class, String text);
-    void trace  (Class<Object> module_class, String text);
-    void info   (Class<Object> module_class, String text);
-    void warn   (Class<Object> module_class, String text);
-    void err    (Class<Object> module_class, String text);
-    void fatal  (Class<Object> module_class, String text);
+    void debug  (Class<? extends OpenBlocksModule> module_class, String text);
+    void trace  (Class<? extends OpenBlocksModule> module_class, String text);
+    void info   (Class<? extends OpenBlocksModule> module_class, String text);
+    void warn   (Class<? extends OpenBlocksModule> module_class, String text);
+    void err    (Class<? extends OpenBlocksModule> module_class, String text);
+    void fatal  (Class<? extends OpenBlocksModule> module_class, String text);
 }
