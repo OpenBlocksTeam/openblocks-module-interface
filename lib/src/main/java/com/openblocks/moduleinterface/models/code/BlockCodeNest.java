8dd8bc3943c17ef5925162ac02c2f768a8eadfe8
Iyxan23 <nurihsanalghifari@gmail.com>
2021-03-24 19:18:24 +0700

    fix: blocks should be public

--- lib/src/main/java/com/openblocks/moduleinterface/models/code/BlockCodeNest.java
+++ lib/src/main/java/com/openblocks/moduleinterface/models/code/BlockCodeNest.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * BlockCodeNest is a nest of {@link BlockCode}, which can be an: If statement, While loop, For loop, function, class, etc
  */
 public class BlockCodeNest extends BlockCode {
-    ArrayList<BlockCode> blocks;
+    public ArrayList<BlockCode> blocks;
 
     public BlockCodeNest(String opcode, ArrayList<String> parameters, ArrayList<BlockCode> blocks) {
         super(opcode, parameters);
