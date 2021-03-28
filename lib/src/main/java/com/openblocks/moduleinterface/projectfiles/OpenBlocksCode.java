8abd47e41aba6ef4f4de84d7264d7a7003cad3ad
Iyxan23 <nurihsanalghifari@gmail.com>
2021-03-28 12:58:36 +0700

    add: Add blocks_format to display blocks

--- lib/src/main/java/com/openblocks/moduleinterface/projectfiles/OpenBlocksCode.java
+++ lib/src/main/java/com/openblocks/moduleinterface/projectfiles/OpenBlocksCode.java
@@ -9,6 +9,9 @@ import java.util.HashMap;
 // TODO: 3/18/21 Should be possible to make multiple collection of blocks / activity
 public class OpenBlocksCode {
     public String block_collection_name;
+    // This HashMap is used by the code editor to display the blocks
+    /** HashMap of opcode, format */
+    public HashMap<String, String> blocks_formats;
     public ArrayList<BlockCode> blocks = new ArrayList<>();
 
     public OpenBlocksCode(String block_collection_name) {
@@ -18,5 +21,9 @@ public class OpenBlocksCode {
     public OpenBlocksCode(String block_collection_name, ArrayList<BlockCode> blocks) {
         this.block_collection_name = block_collection_name;
         this.blocks = blocks;
+    }
+
+    public void setBlocksFormats(HashMap<String, String> blocks_formats) {
+        this.blocks_formats = blocks_formats;
     }
 }
