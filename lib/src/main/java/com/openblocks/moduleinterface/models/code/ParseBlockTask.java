78ce9008185a9ee40164bc1830f23abfa4b60aaf
Iyxan23 <nurihsanalghifari@gmail.com>
2021-04-07 15:11:34 +0700

    fix: parseBlock Should have parameters in it

--- lib/src/main/java/com/openblocks/moduleinterface/models/code/ParseBlockTask.java
+++ lib/src/main/java/com/openblocks/moduleinterface/models/code/ParseBlockTask.java
@@ -1,9 +1,11 @@
 package com.openblocks.moduleinterface.models.code;
 
+import java.util.ArrayList;
+
 /**
  * This interface is used by the compiler to convert a block
  * opcode into java code
  */
 public interface ParseBlockTask {
-    void parseBlock(String code);
+    void parseBlock(String code, ArrayList<String> arguments);
 }
